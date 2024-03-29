21ae9c95b52515b8528d5bb0188ea7b1cc924356
jie <1154311951@qq.com>
2024-03-29 16:02:47 +0800

    Add files via upload

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-accelerate launch --multi_gpu  --config_file accelerate_multi_gpu.yaml $1
+CUDA_VISIBLE_DEVICES=0,1,2 accelerate launch --multi_gpu  --config_file accelerate_multi_gpu.yaml ./pre_train.py > log 2>&1 &
 # accelerate launch $1
 
 # 使用方式，后面接一个训练脚本即可， 如：./train.sh sft.py
